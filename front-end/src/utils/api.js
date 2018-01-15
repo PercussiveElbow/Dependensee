270bd1999a19226f72c2314cd5fe764ea7042f73
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2018-01-15 22:31:54 +0000

    Front end: further work on test

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -61,6 +61,8 @@ function getProjects() {
 }
 
 function postProject(params){
+	    	  console.log(getToken());
+
 	return axios.post(PROJECTS_URL,params, {headers: {'Authorization': getToken()}}).then(response => response.data);
 }
 
--- front-end/test/unit/specs/api.js
+++ front-end/test/unit/specs/api.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
-import {apiSignUp,saveToken,postProject} from '../../../src/utils/api.js'
+import {apiSignUp,saveToken,postProject,getToken} from '../../../src/utils/api.js'
 import 'url-search-params-polyfill';
 var FormData = require('form-data');
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
 
 describe('API Tests', function() {
   describe('#test thing()', function() {
-    it('should sign up correctly and save token', function() {
+    it('should sign up correctly and save token', function(done) {
       	var creds = { name: '', email: '', password: ''};
   		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		for (var i = 0; i < 5; i++)
+		for (var i = 0; i < 8; i++)
 		  creds.name += possible.charAt(Math.floor(Math.random() * possible.length));
 
-		for (var i = 0; i < 5; i++)
+		for (var i = 0; i < 8; i++)
 		  creds.password += possible.charAt(Math.floor(Math.random() * possible.length));
 
 
 
 
-		  creds.email = creds.name + "@demo.com"
+		  creds.email = creds.name + "@test.com"
 
           var formCreds = new URLSearchParams();
           formCreds.append('name', creds.name);
@@ -26,11 +30,10 @@ describe('API Tests', function() {
           formCreds.append('password_confirmation',creds.password);
           var respThing = '';
           console.log("Name: " + creds.name, + " Email: " +creds.email + " Password: " + creds.password);
-          apiSignUp(formCreds).then(resp => (console.log(resp), resp.should.include("Account created"),saveToken(resp.auth_token) ));
+          apiSignUp(formCreds).then(resp => (expect(resp).to.have.property("auth_token") ,saveToken(resp), console.log(getToken()), done() ));
+   			    	this.timeout(10000);
 
-
-
-    });
+    }, 10000);
 
 
     it('should create a project', function() {
@@ -38,6 +41,11 @@ describe('API Tests', function() {
           formCreds.append('name', 'testname');
           formCreds.append('description', 'testdesc');
           formCreds.append('language','Java');
+
+          var formCreds = {}
+          formCreds.name = 'testname'
+          formCreds.description = 'testdesc'
+          formCreds.language = 'Java'
           postProject(formCreds).then(resp => (console.log(resp)));
     });
 
