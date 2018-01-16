9ac760716eb8ef097517e04d4d770bd1d5f27877
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2018-01-16 15:22:13 +0000

    Front end: further updates on api test

--- front-end/test/unit/specs/test_api.js
+++ front-end/test/unit/specs/test_api.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import {apiSignUp,saveToken,postProject,getToken,getProject,getProfile,getCve,deleteProject,upload,getScans,getScan,getDependencies} from '../../../src/utils/api.js'
+import {apiSignUp,saveToken,postProject,getToken,getProject,getProfile,getCve,deleteProject,upload,getScans,getScan,getDependencies,getJsonReport,editProject} from '../../../src/utils/api.js'
 import 'url-search-params-polyfill';
 var FormData = require('form-data');
 var chai = require('chai');
@@ -11,7 +11,7 @@ describe('API Tests', function() {
   	var projectId = ''
     var scanId = ''
 
-    it('should sign up correctly and save token', function(done) {
+    it('should sign up correctly and save token successfully', function(done) {
       	var creds = { name: '', email: '', password: ''};
   		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 		for (var i = 0; i < 8; i++)
@@ -31,15 +31,15 @@ describe('API Tests', function() {
 
     }, 10000);
 
-    it('should get profile sucessfully', function(){
+    it('should get profile successfully', function(){
     	getProfile().then(resp => (expect(resp).to.have.property("name"),expect(resp).to.have.property("email") ) );
     });
 
-    it('should get cve sucessfully', function(){
+    it('should get cve successfully', function(){
     	getCve('2017-14063').then(resp => (expect(resp).to.have.property("desc"), expect(resp).to.have.property("cvss2")));
     });
 
-    it('should create a project and delete a project', function(done) {
+    it('should create a project and delete a project successfully', function(done) {
           var formCreds = {}
           formCreds.name = 'testname'
           formCreds.description = 'testdesc'
@@ -48,7 +48,7 @@ describe('API Tests', function() {
        		this.timeout(10000);
     },10000);
 
-    it('should recreate a project', function(done) {
+    it('should recreate a project successfully', function(done) {
           var formCreds = {}
           formCreds.name = 'testname'
           formCreds.description = 'testdesc'
@@ -57,26 +57,38 @@ describe('API Tests', function() {
        		this.timeout(10000);
     },10000);
 
-    it('should get a project', function() {
+    it('should get a project successfully', function() {
           getProject(projectId).then(resp => (expect(resp).to.have.property("id")));
     });
 
-    it('should upload a test file for a new scan', function(done) {
+    it('should edit a project successfully', function() {
+    	  var formCreds = {}
+          formCreds.name =  'newname';
+          formCreds.description = 'newdesc';
+          editProject(projectId,formCreds).then( resp => { getProject(projectId).then(resp => (expect(resp.name).to.have.equal("newname")));});
+    });
+
+    it('should upload a test file for a new scan successfully', function(done) {
 	  var contents = fs.readFileSync('test/unit/specs/pom.xml.test', 'utf8');
       upload(projectId,contents,"Web [TestRun] ").then(resp => { expect(resp).to.have.property("scan_id"), expect(resp).to.have.property("dependencies"),expect(resp).to.have.property("type"), scanId = resp['scan_id'],done()    });
       this.timeout(10000);
     },10000);
 
-    it('should appear as a scan under projects', function() {
+    it('should appear as a scan under projects successfully', function() {
     	getScans(projectId).then(resp => {expect(resp.length).to.equal(1)});
     });
 
-    it('should get the individual scan', function() {
+    it('should get the individual scan successfully', function() {
     	getScan(projectId,scanId).then(resp => {expect(resp).to.have.property("id")});
     });
 
-    it('should get dependencies', function() {
+    it('should get dependencies successfully', function() {
     	getDependencies(projectId,scanId).then(resp => { expect(resp.length).to.equal(6) });
     });
+
+    it('should get vulnerabilities successfully', function() {
+    	getJsonReport(projectId,scanId).then(resp => { expect(resp).to.have.property("org.asynchttpclient.async-http-client") });
+    });
+
 
   });
