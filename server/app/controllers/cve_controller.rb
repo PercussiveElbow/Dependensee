29b2c0ece4710897510d55d93a5d0feca17881f1
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2018-03-21 23:09:59 +0000

    Further improvements for codeclimate

--- server/app/controllers/cve_controller.rb
+++ server/app/controllers/cve_controller.rb
@@ -23,25 +23,32 @@ class CveController < ApplicationController
   # end
 
   def find_cve_by_id
+      param_validate
+      @cve = RubyCve.where(['cve_id = ?', params[:id]])
+      if !set_lang?('Ruby')
+        @cve = JavaCve.where(['cve_id = ?', params[:id]])
+        if !set_lang?('Java')
+          @cve = PythonCve.where(['cve_id = ?', params[:id]])
+            set_lang?('Python')
+        end
+      end
+  end
+
+  def param_validate
     begin
       param! :id, String, required: true, format:  /^\d{4}-(0\d{3}|[1-9]\d{3,})$/
     rescue
       raise CustomException::ValidationError, MsgConstants::VALIDATION_ERROR
     end
-      @cve = RubyCve.where(['cve_id = ?', params[:id]])
-      if !@cve.nil? and !@cve.empty?
-        @language = 'Ruby'
-      else
-        @cve = JavaCve.where(['cve_id = ?', params[:id]])
-        if !@cve.nil? and !@cve.empty?
-          @language = 'Java'
-        else
-          @cve = PythonCve.where(['cve_id = ?', params[:id]])
-          if !@cve.nil? and !@cve.empty?
-            @language = 'Python'
-          end
-        end
-      end
+  end
+
+  def set_lang?(lang)
+    if !@cve.nil? and !@cve.empty?
+      @language = lang
+      true
+    else
+      false
+    end
   end
 
   # def find_cve_by_name
