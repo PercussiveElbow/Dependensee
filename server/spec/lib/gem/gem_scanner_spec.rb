866b0a6da8848f6fb7d8bb6b860229ecd71ec83e
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2019-10-07 23:17:20 +0100

    Updating dependencies, meta

--- server/spec/lib/gem/gem_scanner_spec.rb
+++ server/spec/lib/gem/gem_scanner_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe 'GemScanner' do
   it 'should be created and scan correctly' do
     parser = GemParser::load_from_post(File.read File.expand_path(File.dirname(__FILE__) + '../../../resources/Gemfile.lock.test'))
     scanner = GemScanner::new(parser.load_deps)
-    expect(scanner.scan.length).to eql(4)
+    expect(scanner.scan.length).to eql(5)
   end
 end
